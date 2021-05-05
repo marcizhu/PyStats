5e299615d23e77df2d4feeb4c24c872c0f4a7149
marcizhu <marc.izquierdo.hu@gmail.com>
2021-05-05 08:52:45 +0200

    Added docs for Binomial distribution

---
+++ docs/source/api/Binomial.rst
@@ -0,0 +1,106 @@
+.. Copyright (c) 2011-2021 Keith O'Hara
+
+   Distributed under the terms of the Apache License, Version 2.0.
+
+   The full license is in the file LICENSE, distributed with this software.
+
+Binomial Distribution
+=====================
+
+**Table of contents**
+
+.. contents:: :local:
+
+----
+
+Density Function
+----------------
+
+The density function of the Binomial distribution:
+
+.. math::
+
+   f(x; n, p) = \binom{n}{x} p^x (1-p)^{n-x} \times \mathbf{1}[x \in \{0,\ldots,n\}]
+
+Methods for scalar input, as well as for list input, are listed below.
+
+Scalar Input
+~~~~~~~~~~~~
+
+.. _dbinom-func-ref1:
+.. autofunction:: pystats.dbinom(x: float, n_trials: int, prob: float, log: bool = False) -> float
+
+List Input
+~~~~~~~~~~
+
+.. _dbinom-func-ref2:
+.. autofunction:: pystats.dbinom(x: List[float], n_trials: int, prob: float, log: bool = False) -> List[float]
+
+----
+
+Cumulative Distribution Function
+--------------------------------
+
+The cumulative distribution function (CDF) of the Binomial distribution:
+
+.. math::
+
+   F(x; n, p) = \sum_{z \leq x} f(z; n, p)
+
+Methods for scalar input, as well as for list input, are listed below.
+
+Scalar Input
+~~~~~~~~~~~~
+
+.. _pbinom-func-ref1:
+.. autofunction:: pystats.pbinom(p: float, n_trials: int, prob: float, log: bool = False) -> float
+
+List Input
+~~~~~~~~~~
+
+.. _pbinom-func-ref2:
+.. autofunction:: pystats.pbinom(p: List[float], n_trials: int, prob: float, log: bool = False) -> List[float]
+
+----
+
+Quantile Function
+-----------------
+
+The quantile function of the Binomial distribution:
+
+.. math::
+
+   q(r; n, p) = \inf \left\{ x : r \leq F(x; n, p) \right\}
+
+Methods for scalar input, as well as for list input, are listed below.
+
+Scalar Input
+~~~~~~~~~~~~
+
+.. _qbinom-func-ref1:
+.. autofunction:: pystats.qbinom(q: float, n_trials: int, prob: float) -> float
+
+List Input
+~~~~~~~~~~
+
+.. _qbinom-func-ref2:
+.. autofunction:: pystats.qbinom(q: List[float], n_trials: int, prob: float) -> List[float]
+
+----
+
+Random Sampling
+---------------
+
+Random sampling for the Binomial distribution is achieved by summing the results of simulating `n` Bernoulli-distributed random variables.
+
+Scalar Output
+~~~~~~~~~~~~~
+
+.. _rbinom-func-ref1:
+.. autofunction:: pystats.rbinom(n_trials: int, prob: float) -> float
+
+List Output
+~~~~~~~~~~~
+
+.. _rbinom-func-ref3:
+.. autofunction:: pystats.rbinom(n: int, n_trials: int, prob: float) -> List[float]
--- src/Binomial.cpp
+++ src/Binomial.cpp
@@ -68,7 +68,7 @@ void Binomial(py::module& m)
 		"    prob (float): The probability parameter, a real-valued input.\n\n"
 
 		"Returns:\n"
-		"    A vector of pseudo-random draws from the Binomial distribution.");
+		"    A list of pseudo-random draws from the Binomial distribution.");
 
 	// Vector input
 	m.def("dbinom", static_cast<std::vector<double>(*)(const std::vector<stats::llint_t>&, stats::llint_t, double, bool)>(&stats::dbinom),
@@ -76,36 +76,36 @@ void Binomial(py::module& m)
 		"Density function of the Binomial distribution.\n\n"
 
 		"Args:\n"
-		"    x (List[int]): A standard vector input.\n"
+		"    x (List[int]): A standard list input.\n"
 		"    n_trials (int): The number of trials, a non-negative integral-valued input.\n"
 		"    prob (float): The probability parameter, a real-valued input.\n"
 		"    log (bool): Return the log-density or the true form.\n\n"
 
 		"Returns:\n"
-		"    A vector of density values corresponding to the elements of `x`.");
+		"    A list of density values corresponding to the elements of `x`.");
 
 	m.def("pbinom", static_cast<std::vector<double>(*)(const std::vector<double>&, stats::llint_t, double, bool)>(&stats::pbinom),
 		py::arg("p"), py::arg("n_trials"), py::arg("prob"), py::arg("log") = false,
 		"Distribution function of the Binomial distribution.\n\n"
 
 		"Args:\n"
-		"    p (List[float]): A standard vector input.\n"
+		"    p (List[float]): A standard list input.\n"
 		"    n_trials (int): The number of trials, a non-negative integral-valued input.\n"
 		"    prob (float): The probability parameter, a real-valued input.\n"
 		"    log (bool): Return the log-density or the true form.\n\n"
 
 		"Returns:\n"
-		"    A vector of CDF values corresponding to the elements of `p`.");
+		"    A list of CDF values corresponding to the elements of `p`.");
 
 	m.def("qbinom", static_cast<std::vector<double>(*)(const std::vector<double>&, stats::llint_t, double)>(&stats::qbinom),
 		py::arg("q"), py::arg("n_trials"), py::arg("prob"),
 		"Quantile function of the Binomial distribution.\n\n"
 
 		"Args:\n"
-		"    q (List[float]): A standard vector input.\n"
+		"    q (List[float]): A standard list input.\n"
 		"    n_trials (int): The number of trials, a non-negative integral-valued input.\n"
 		"    prob (float): The probability parameter, a real-valued input.\n\n"
 
 		"Returns:\n"
-		"    A vector of quantiles values corresponding to the elements of `q`.");
+		"    A list of quantiles values corresponding to the elements of `q`.");
 }
